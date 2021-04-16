a2e93d2669c310d9fc893145a234e8b051e2a060
Xuffasch <tristan.lucas@gmail.com>
2021-04-16 11:20:40 +0200

    Append explanation to writable store subscription

--- src/components/stores/1-writable/Output.svelte
+++ src/components/stores/1-writable/Output.svelte
@@ -6,6 +6,8 @@
 
   onMount( () => {
     // The function given to the Svelte Writable store should take as input the stored value 'value' which will be used to update a local variable '_value', to be display in an element as an example below.
+    // the Svelte store API returns a function when we subscribe to the writableStore. That function is used to unsubscribe the provided function from the store and avoid memory leak.
+    // Since the unsubscribe function is returned to the unMount function, it will be called when this component is unmounted which effectively unsubscribe the input function from the store.
     return writableStore.subscribe( value => {
       _value = value + " and Chorizo Pizza !";
     })
