571e2039e6842ad7efd8b62030a873e64bdcb548
Xuffasch <tristan.lucas@gmail.com>
2021-04-16 11:04:15 +0200

    Explain  subscription func send to writable store

--- src/components/stores/1-writable/Output.svelte
+++ src/components/stores/1-writable/Output.svelte
@@ -5,6 +5,7 @@
   let _value;
 
   onMount( () => {
+    // The function given to the Svelte Writable store should take as input the stored value 'value' which will be used to update a local variable '_value', to be display in an element as an example below.
     return writableStore.subscribe( value => {
       _value = value + " and Chorizo Pizza !";
     })
